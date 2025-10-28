071e2a63fc32e320a0f5c280ddc50257a46f5fff
Jonas Kessler <kesslejo@gmx.de>
2025-10-28 11:13:28 +0100

    added logger

--- app.py
+++ app.py
@@ -28,6 +28,25 @@ from flask import (
     url_for,
 )
 
+from logging.config import dictConfig
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
 OTHER_BANK_IPS: list[str] = ["91.98.115.224"]
 
 
